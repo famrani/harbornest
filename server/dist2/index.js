71f3c0e01173b2fef7ee51e8c6fb228b3d88df3b
Faycal Amrani <famrani@alldigitalnetwork.com>
2025-11-18 12:53:19 +0100

    Stop tracking dist2 and env, add dist2 to .gitignore

--- server/dist2/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-const main_1 = require("./app/main/main");
-let app = new main_1.MainComponent();
-exports.app = app;
-
-//# sourceMappingURL=index.js.map
